77f9804b58883ed2f47d0ec2a49dd35e37c6c4d5
Kateryna Miroievska <miroevskaya.k@gmail.com>
2023-01-25 21:50:33 +0100

    find the correct article

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 "use strict";
 
 function titleClickHandler(event) {
+  event.preventDefault();
   const clickedElement = this;
-  console.log("clickedElement (with plus): " + clickedElement);
 
   /* [DONE] remove class 'active' from all article links  */
 
@@ -15,8 +15,6 @@ function titleClickHandler(event) {
   /* [IN PROGRESS] add class 'active' to the clicked link */
   clickedElement.classList.add("active");
 
-  console.log("clickedElement:", clickedElement);
-
   /* remove class 'active' from all articles */
   const activeArticles = document.querySelectorAll(".posts article.active");
 
@@ -24,10 +22,14 @@ function titleClickHandler(event) {
     activeArticle.classList.remove("active");
   }
   /* get 'href' attribute from the clicked link */
+  const clickedElAttr = clickedElement.getAttribute("href");
+  console.log(clickedElAttr);
 
   /* find the correct article using the selector (value of 'href' attribute) */
-
+  const articleToActive = document.querySelector(clickedElAttr);
+  console.log(articleToActive);
   /* add class 'active' to the correct article */
+  articleToActive.classList.add("active");
 }
 
 const links = document.querySelectorAll(".titles a");
